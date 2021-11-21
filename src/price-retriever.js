bf12ce3905c6691867c53f5190c4cc998fa3cb30
Niklas Borglund <niklasborglund@gmail.com>
2021-11-21 12:15:28 +0100

    Add basic price retriever class that can fetch historic prices from coin gecko

---
+++ src/price-retriever.js
@@ -0,0 +1,74 @@
+﻿'use strict'
+const fs = require('fs');
+const path = require("path");
+const colors = require('colors');
+const coinGeckoClient = new (require('coingecko-api'))();
+
+exports.priceRetriever = function(){
+    const coinDatabasePath = __dirname + '/../data/coingecko-all-coins.json';
+    const historyFolderPath = __dirname + '/../data/price';
+    
+    this.getPrice = async function(name, tickerSymbol, dateString, currency){
+        let coinInfo = await this.getCoinInfo(name, tickerSymbol);
+        let historyData = await this.retrieveHistory(coinInfo, dateString);
+
+        let nameSection = coinInfo.name.bold + '(' + tickerSymbol.yellow + ')';
+        let dateSection = 'date:' + dateString;
+        let priceSection = 'price:' + (' ' + historyData.data.market_data.current_price[currency] + ' ').green + currency;
+        
+        console.log(nameSection + " " + dateSection + " " + priceSection);
+    };
+    
+    this.retrieveHistory = async function(coinInfo, dateString)
+    {
+        let fullPath = historyFolderPath + '/' + coinInfo.symbol + '/' + dateString + '.json';
+
+        let history = undefined;
+        if(fs.existsSync(fullPath)) {
+           // console.log('cached coin history exists. Using that one instead.');
+            history = JSON.parse(fs.readFileSync(fullPath));
+        }
+        else {
+           // console.log('retrieving history from coin gecko for date ' + dateString + '...');
+            history = await coinGeckoClient.coins.fetchHistory(coinInfo.id, {
+                date: dateString,
+                localization: false
+            });
+            
+            this.ensureDirectoryExistence(fullPath);
+            fs.writeFileSync(fullPath,  JSON.stringify(history, null, 2));
+        }
+        return history;
+    }
+    
+    this.getCoinInfo = async function(name, tickerSymbol){
+        let allCoins = await this.getCoinDatabase();
+        return allCoins.data.find(o => o.name.toLowerCase() === name.toLowerCase() && o.symbol.toLowerCase() === tickerSymbol.toLowerCase());
+    }
+    
+    this.getCoinDatabase = async function () {
+        let allCoins = undefined;
+        if(fs.existsSync(coinDatabasePath)) {
+            //console.log('cached coin list exists. Using that one instead.');
+            allCoins = JSON.parse(fs.readFileSync(coinDatabasePath));
+        }
+        else {
+            //console.log('retrieving list of coins from coin gecko...');
+            allCoins = await coinGeckoClient.coins.list();
+            //console.log('done!');
+            this.ensureDirectoryExistence(coinDatabasePath);
+            fs.writeFileSync(coinDatabasePath,  JSON.stringify(allCoins, null, 2));
+        }
+        
+        return allCoins;
+    };
+
+    this.ensureDirectoryExistence = function(filePath) {
+        let dirname = path.dirname(filePath);
+        if (fs.existsSync(dirname)) {
+            return true;
+        }
+        this.ensureDirectoryExistence(dirname);
+        fs.mkdirSync(dirname);
+    }
+};
