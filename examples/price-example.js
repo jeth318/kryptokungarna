ee9dcd81ec1e7ecac65eb877d182c57554e723e1
Niklas Borglund <niklasborglund@gmail.com>
2021-11-21 12:15:57 +0100

    Add test class with examples on how to use the price retriever

---
+++ examples/price-example.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+var priceRetriever = new (require('../src/price-retriever').priceRetriever)();
+
+function getPrices() {
+    priceRetriever.getPrice('Avalanche','AVAX', '20-11-2021', 'sek');
+    priceRetriever.getPrice('Cardano','ADA', '16-01-2019', 'sek');
+    priceRetriever.getPrice('Litecoin','LTC', '5-09-2020', 'sek');
+    priceRetriever.getPrice('VeChain','VET', '8-08-2020', 'sek');
+    priceRetriever.getPrice('Uniswap','UNI', '26-05-2021', 'sek');
+}
+
+getPrices();
